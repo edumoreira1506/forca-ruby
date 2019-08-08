1a5b5d6ebf3856d45d01bb6f0139febf41e514bb
edumoreira1506 <contato@eduardoem.com.br>
2019-08-08 10:09:47 -0300

    Separating all logic game from panel and code optimization

--- game.rb
+++ game.rb
@@ -8,10 +8,16 @@ class Game
     end
 
     def start
+        @chances = 3
+
         @game_over = false
+        @last_try = false
         @choice_category = nil
+
+        @chars = []
+        @chosen_letters = []
         
-        @panel.welcome_text
+        @panel.welcome_text(@points)
         
         @categories.each_with_index { |category, index| puts "#{index} - #{category.name}" }
         
@@ -20,11 +26,36 @@ class Game
     end
 
     private
+        def update_letter
+            letter = @chosen_letters.last
+
+            if @word.include? letter
+
+                if @last_try && @chances < 3
+                    @chances += 1
+                end
+
+                i = 0
+                while i < @word.length
+                    if(@word[i] == letter)
+                        @chars[i] = letter
+                    end
+                    
+                    i += 1
+                end
+            else
+                @chances -= 1
+            end
+
+            @last_try = @word.include? letter
+        end
+
         def valid_category?(index_category)
             if @categories[index_category]
                 @choice_category = @categories[index_category]
 
                 randomize_word
+                crypt_word
                 make_play
             else
                 start
@@ -32,77 +63,68 @@ class Game
         end
 
         def randomize_word
-            amount_of_words = @choice_category.words.length
-            random_index = rand(0..amount_of_words - 1)
-            word = @choice_category.words[random_index]
-            @panel.start(word)
+            @word = @choice_category.words.sample
+        end
+
+        def crypt_word
+            while @word.length > @chars.length do
+                @chars.push('_') 
+            end
         end
 
         def make_play
-            @panel.show_panel
+            @panel.show_panel(@chances, @chosen_letters, @chars)
             
             puts 'Next letter?'
             try = gets.chomp
 
-            if ''.casecmp(try) == 0 || ' '.casecmp(try) == 0
-                make_play
-            else
-                if'GUESS'.casecmp(try) == 0
-                    puts 'What is your guess?'
-                    guess = gets.chomp
-    
-                    @game_over = true
-
-                    if@panel.word.casecmp(guess) == 0
-                        @points = @points + 100
-                        @lost = false
-                    else
-                        @lost = true
-                    end
+            if try.length == 1
+                if@chosen_letters.include? try
+                    puts 'You already tried this letter'
                 else
-                    if@panel.chosen_words.include? try
-                        puts 'You already tried this letter'
-                    else
-                        @panel.increment_panel(try)
-                    end
+                    @chosen_letters.push(try)
+                    update_letter
                 end
-    
-                check_game_over
-    
-                if(@game_over)
-                    end_game
+            else
+                @game_over = true
+
+                if@word.casecmp(try) == 0
+                    @points = @points + 100
+                    @lost = false
                 else
-                    make_play
+                    @lost = true
                 end
             end
+    
+            check_game_over
+            @game_over ? end_game : make_play
         end
 
         def check_game_over
             if !@game_over
-                if @panel.chances == 0
+                if @chances == 0
                     @game_over = true
                     @lost = true
-                end
-
-                if !@panel.chars.include? '_'
+                elsif !@chars.include? '_'
                     @game_over = true
                     @lost = false
                 end
             end
         end
 
-        def break_line
-            puts '==================='
-        end 
-
         def end_game
-            percentage_wrong_letters =  (@panel.wrong_letters * 100) / @panel.total_plays 
+            total_letters = @chosen_letters.length
+            wrong_letters = 0
+            right_letters = 0
+
+            @chosen_letters.each { |letter|  (@word.include? letter) ? right_letters +=  1 : wrong_letters += 1 }
+
+            percentage_wrong_letters = (wrong_letters * 100) / total_letters 
             percentage_right_letters = 100 - percentage_wrong_letters
 
             @points += percentage_right_letters - percentage_wrong_letters
 
-            message = @lost ? 'You loose' : 'You win'
-            puts "Game ended! The word was: #{@panel.word}. #{message}"
+            @panel.end_game(@lost, @word)
             sleep(1)
             start
         end
--- panel.rb
+++ panel.rb
@@ -1,69 +1,27 @@
 class Panel
     attr_reader :wrong_letters, :right_letters, :chars, :word, :chances, :chosen_words
 
-    def welcome_text
+    def welcome_text(points)
         puts 'Welcome to hangman game! Rules:'
         puts 'You must choice one of 4 categories to start play'
         puts 'You have 3 chances for win. One wrong choice and you lost one chance, but if you hit two times you win one chance, anyway the maximum of chances is 3.'
         puts 'Anytime you can try to guess the word typing GUESS, but if you are wrong, you lost.'
 
         break_line
-        puts "You have #{@points} points"
+        puts "You have #{points} points"
         break_line
     end
 
-    def start(word)
-        @last_try = false
-        @word = word
-        @wrong_letters = 0
-        @right_letters = 0
-        @chars = []
-        @chosen_words = []
-        @chances = 3
-        clean_panel
-    end
-
-    def increment_panel(letter)
-        @chosen_words.push(letter)
-
-        if word.include? letter
-            @right_letters += 1
-
-            if @last_try && @chances < 3
-                @chances += 1
-            end
-
-            i = 0
-            while i < @word.length
-                if(@word[i] == letter)
-                    @chars[i] = letter
-                end
-                
-                i += 1
-            end
-        else
-            @wrong_letters += 1
-            @chances -= 1
-        end
-
-        @last_try = word.include? letter
-    end
-
-    def clean_panel
-        while @word.length > @chars.length do
-            @chars.push('_') 
-        end
-    end
-
-    def total_plays
-        @wrong_letters + @right_letters
-    end
-
-    def show_panel
+    def show_panel(chances, chosen_words, chars)
         puts '==================='
         puts "You have #{chances} chances."
         puts "Your tries: #{chosen_words.to_s}."
         puts "Word: #{chars.join('')}."
+    end
+
+    def end_game(lost, word)
+        message = lost ? 'You loose' : 'You win'
+        puts "Game ended! The word was: #{word}. #{message}"
     end
 
     private
